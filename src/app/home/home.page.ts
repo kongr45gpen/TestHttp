8839a5aa5ae32ec2e3a799805d5bd24547de484d
kongr45gpen <electrovesta@gmail.com>
2020-10-13 05:20:11 +0300

    Test with ODTDocument

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { HTTP } from '@ionic-native/http/ngx';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+import { Zip } from '@ionic-native/zip/ngx';
+import docx4js from "docx4js"
+import ModelHandler from "docx4js";
+import mammoth from "mammoth/mammoth.browser";
+import ODTDocument from "./ODTDocument.js";
+
 
 @Component({
   selector: 'app-home',
@@ -8,6 +15,28 @@ import { HTTP } from '@ionic-native/http/ngx';
 })
 export class HomePage {
 
-  constructor(private http: HTTP) {}
+  constructor(private zip: Zip) { }
 
+  ionViewWillEnter() {
+
+    console.log("hello")
+    console.log(ODTDocument);
+
+    var req = new XMLHttpRequest();
+    req.open('GET', '/assets/test.odt');
+    req.responseType = 'arraybuffer';
+    req.addEventListener('load', function() {
+      var empty = req.response;
+      
+      var odtdoc = new ODTDocument(empty);
+      try {
+        var html = odtdoc.getHTMLUnsafe();
+        console.log(html);
+      } catch(e) {
+        throw e;
+      }
+      var odt = odtdoc.getODT();
+    });
+    req.send();
+}
 }
